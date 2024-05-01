ff0c420fdf01c74e5829041e91d8388d8d3c7945
Denillox <165029562+Denillox@users.noreply.github.com>
2024-05-01 20:31:16 +0200

    Initialt

--- DAT055/src/main/java/org/example/ChatApplication.java
+++ DAT055/src/main/java/org/example/ChatApplication.java
@@ -1,4 +1,49 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatApplication {
+    private List<User> users;
+    private ChatHistory chatHistory;
+
+    public ChatApplication() {
+        this.users = new ArrayList<>();
+        this.chatHistory = new ChatHistory();
+    }
+
+    /*
+    public void startSession() {
+        users = new ArrayList<>();
+        chatHistory = new ChatHistory();
+    }
+    */
+    
+    public void endSession() {
+        updateChatHistory();
+        users.clear();
+    }
+
+    public void addUser(User user) {
+        if (!users.contains(user)) {
+            users.add(user);
+        }
+    }
+
+    public void removeUser(User user) {
+        if (users.contains(user)) {
+            users.remove(user);
+        }
+    }
+
+    public void broadcastMessage(Message message) {
+        chatHistory.addMessage(message);
+        message.send();
+    }
+
+    public void broadcastMMS(MMS mms) {
+        chatHistory.addMessage(mms);
+        mms.send();
+    }
+
 }
