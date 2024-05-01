56b0377647c5a3b424dbebbc9b69e8332702c991
Denillox <165029562+Denillox@users.noreply.github.com>
2024-05-01 20:33:45 +0200

    Initialt

--- DAT055/src/main/java/org/example/User.java
+++ DAT055/src/main/java/org/example/User.java
@@ -1,4 +1,35 @@
 package org.example;
+import java.io.File;
 
 public class User {
+    private String username;
+    private ChatApplication chatApp; 
+
+    public User(String username, ChatApplication chatApp) {
+        this.username = username;
+        this.chatApp = chatApp;
+    }
+
+    public void connectToChat() {
+        chatApp.addUser(this);
+    }
+
+    public void disconnectFromChat() {
+        chatApp.removeUser(this);
+    }
+
+    public void sendTextMessage(String text) {
+        Message message = new TextMessage(text, this);  //Ändra i linje med textmessage/message
+        chatApp.broadcastMessage(message);
+    }
+
+    public void sendMMS(File imageFile) {
+        MMS mms = new MMS(imageFile, this);  //Ändra i linje med mms
+        chatApp.broadcastMMS(mms);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
 }
